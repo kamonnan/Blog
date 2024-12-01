1dc71aef86a9f4d809ddb88bf3cabfe3882e1d0d
kamonnan <kamonnan@kkumail.com>
2024-12-01 15:33:07 +0700

    updates

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,10 @@ class CommentsController < ApplicationController
       redirect_to article_path(@article)
     end
   
-    private
-      def comment_params
-        params.expect(comment: [:commenter, :body])
-      end
+ 
+    private def comment_params
+      params.expect(comment: [:commenter, :body, :status])
+    end
+
   end
   
