3df8cfcbf18cc15f37c4944538516785d669a42a
kamonnan <kamonnan@kkumail.com>
2024-12-01 13:24:15 +0700

    updates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,29 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  class ArticlesController < ApplicationController
+    def index
+      @articles = Article.all
+    end
+  
+    def show
+      @article = Article.find(params[:id])
+    end
+  
+    def new
+      @article = Article.new
+    end
+  
+    def create
+      @article = Article.new(title: "...", body: "...")
+  
+      if @article.save
+        redirect_to @article
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+  end
+  
 end
